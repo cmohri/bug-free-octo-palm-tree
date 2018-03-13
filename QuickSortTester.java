f2118fb50493816e82a49b186c5655d0540ee547
John Liu <33496334+jliu01298@users.noreply.github.com>
2018-03-13 13:46:43 -0400

    Update QuickSortTester.java

--- QuickSortTester.java
+++ QuickSortTester.java
@@ -1,5 +1,21 @@
 public class QuickSortTester{
 
+    public static int[] buildArray(int size){
+	int[] retArr= new int [size];
+	for(int i=0; i<s;i++){
+	    retArr[i]=i;
+	}
+	return retArr;
+    }
+    public static void shuffle( int[] d ) {
+	int tmp;
+	int swapPos;
+	for( int i = 0; i < d.length; i++ ) {
+	    tmp = d[i];
+	    swapPos = i + (int)( (d.length - i) * Math.random() );
+	    swap( i, swapPos, d );
+	}
+    }
     public static void main (String[] args){
 
     }
